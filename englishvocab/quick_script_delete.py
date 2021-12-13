35adb621326002281062acccd3380acc13ec96d0
Noorudin Buraleh <noorudin.buraleh@gmail.com>
2021-12-13 19:18:19 +0000

    SQUIZ: Remove old django setup, new direction is simple python dict access for recall

--- englishvocab/quick_script_delete.py
+++
@@ -1,8 +0,0 @@
-with open('words.csv', 'r') as fo:
-    counter = 0
-    for line in fo.readlines():
-        # import ipdb; ipdb.set_trace()
-        if line.count(',') > 5:
-            counter += 1
-
-    print(counter)
