4e88e4b9d27518251c02b54a6b1a93db2e6a4c56
Noorudin Buraleh <noorudin.buraleh@gmail.com>
2021-12-22 14:31:27 +0000

    SQUIZ: Initial Setup for Advanced C++ Course Notes

--- qna_mapping.py
+++ qna_mapping.py
@@ -1,10 +1,12 @@
 from qna import c_basics
 from qna import cpp_basics
 from qna import gregoire # from 'Professional C++'
+from qna import cisco_advanced_cpp # from Cisco Academy's 'Advanced C++'
 
 
 qna_mapping = {
     1: c_basics.qna,
     2: cpp_basics.qna,
     3: gregoire.qna,
+    4: cisco_advanced_cpp.qna,
 }
