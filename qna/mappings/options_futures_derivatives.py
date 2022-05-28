958f410251a173fe3bc363bfd8badb817566f3c9
nooraleh <57625672+nooraleh@users.noreply.github.com>
2022-05-28 17:49:15 +0100

    main: Chapter 1 - Options, Futures, and Other Derivatives

---
+++ qna/mappings/options_futures_derivatives.py
@@ -0,0 +1,711 @@
+qna = {
+    1: {
+		'q':  """
+		What is a derivatives exchange?
+		""",
+		'a': """
+		A market where individuals and companies trade standardized contracts
+		that have been defined by the exchange.
+		""",
+	},
+    2: {
+		'q':  """
+		i)  What is a leverage ratio?
+		ii) State a few common leverage ratios
+		""",
+		'a': """
+		i) A leverage ratio is any of the several financial
+			measurements that assesses the ability of a company to meet
+			its financial obligations.
+
+		ii) Debt-equity, equity multiplier, degree of financial leverage
+			consumer leverage ratio.
+		""",
+	},
+    3: {
+		'q':  """
+		In the context of financial institutions, what is systematic risk?
+		""",
+		'a': """
+		Systematic risk is the risk that a default by one financial institution
+		will create a "ripple effect" that leads to defaults by other financial
+		institutions and threatens the stability of the financial system.
+		""",
+	},
+    4: {
+		'q':  """
+		In the context of the OTC market, what is compression? 
+		""",
+		'a': """
+		Compression is a procedure where two or more counterparties restructure
+		transactions with each other with the result that the underlying principal
+		is reduced. The net risk is the same before and after restructuring.
+		""",
+	},
+    5: {
+		'q':  """
+		What is a forward contract?
+		""",
+		'a': """
+		It is an agreement to buy or sell an asset at a certain future time
+		for a certain price.
+		""",
+	},
+    6: {
+		'q':  """
+		What is a spot contract?
+		""",
+		'a': """
+		A spot contract is an agreement to buy or sell an asset almost
+		immediately.
+		""",
+	},
+    7: {
+		'q':  """
+		Fill in the blank:
+
+		One of the parties to a forward contract assumes a <_____> position and 
+		agrees to buy the asset on a certain specified future date for
+		a certain specified price.
+
+		The other party assumes a <____> position and agrees to sell the asset
+		on the same date for the same price.
+		""",
+		'a': """
+		1) long
+		2) short
+		""",
+	},
+    8: {
+		'q':  """
+		Give the generalized formula for the payoff from:
+
+			1) a long position in a forward contract on one unit of an asset
+			2) a short position in a forward contract on one unit of an asset
+		""",
+		'a': """
+			1) S_T - K
+			2) K - S_T
+
+		Where:
+			- S_T is the spot price of the asset at maturity of the contract
+			- K is the delivery price
+		""",
+	},
+    9: {
+		'q':  """
+		What day is the precise expiration date for monthly options?
+		""",
+		'a': """
+		The third Friday of the expiration month.
+		""",
+	},
+    10: {
+		'q':  """
+		Explain how the strike price and time to maturity affect the price/value
+		of call and put options respectively.
+		""",
+		'a': """
+		Call options:
+			- as the stike price increases, the price of the option decreased
+			- as time to maturity increases, the price of the option increases
+
+		Put options:
+			- as the strike price incerases, the price of the option increases
+			- as time to maturity increases, the price of the option increases
+		""",
+	},
+	11: {
+		'q':  """
+		Selling an option is also know as?
+		""",
+		'a': """
+		Writing the option.
+		""",
+	},
+	12: {
+		'q':  """
+		What are the three categories of traders? Breifly describe why each categories
+		generally use derivatives?
+		""",
+		'a': """
+			1) Hedgers - use derivatives to reduce the risk that they face from
+				potential future movements in a market variable.
+			2) Speculators - use derivatives to bet on the future direction of a market
+						variable
+			3) Arbitrageurs - take offsetting positions in two or more instruments to
+					lock in a profit.
+		""",
+	},
+	13: {
+		'q':  """
+		Consider the hedge fund strategy 'long/short equities'. What does this entail?
+		""",
+		'a': """
+		Purchase securities considered to be undervalued and short those considered
+		to be overvalued in such a way that the exposure to the overall direction
+		of the market is small.
+		""",
+	},
+	14: {
+		'q':  """
+		Consider the hedge fund strategy 'convertible arbitrage'. What does this entail?
+		""",
+		'a': """
+		Taking a long position in a thought-to-be-undervalued convertible bond combined
+		with an actively managed short position in the underlying equity.
+		""",
+	},
+	15: {
+		'q':  """
+		Consider the hedge fund strategy 'distressed securities'. What does this entail?
+		""",
+		'a': """
+		Buying securities issue by companies in, or close to, bankruptcy.
+		""",
+	},
+	16: {
+		'q':  """
+		Consider the hedge fund strategy 'emerging markets'. What does this entail?
+		""",
+		'a': """
+		Invest in debt and equity of companies in developing or emerging countries
+		and in the debt of the countries themselves.
+		""",
+	},
+	17: {
+		'q':  """
+		Consider the hedge fund strategy 'global macro'. What does this entail?
+		""",
+		'a': """
+		Carry out trades that reflect anticipated global macroeconomic trends.
+		""",
+	},
+	18: {
+		'q':  """
+		Consider the hedge fund strategy 'merger arbitrage'. What does this entail?
+		""",
+		'a': """
+		Trade after a possible merger or acquisition is announced so that a 
+		profit is made if the announced deal takes place.
+		""",
+	},
+	19: {
+		'q':  """
+		Explain carefully the difference between selling a call option and buying a put option
+		Express the payoff mathematically in each case.
+		""",
+		'a': """
+		Selling a call option involves giving someone else the right to buy an asset from you. It gives
+		you a payoff of:
+
+			-max(S_{T}-K, 0) = min(K-S_{T}, 0)
+
+		Buying a put option involves buying an option from someone else. It gives a payoff of:
+
+			max(K-S_{T}, 0)
+
+		In both cases, the potential payoff is (K - S_{T}).
+		
+		When you write a call option, the payoff is negative or zero.
+		(This is because the counterparty chooses whether to exercise.)
+
+		When you buy a put option, the payoff is zero or positive.
+		(This is because you choose whether to exercise.)
+		""",
+	},
+	20: {
+		'q':  """
+		What is one way of differentiating the use of futures contracts for hedging
+		vs. speculation?
+		""",
+		'a': """
+		One key difference is when the party entering into the contract is exposed to
+		the price of the underlying asset.
+
+		Examples:
+			A corn farmer is exposed to price movements of corn.
+			An energy corporation is exposed to the price of oil etc.
+		""",
+	},
+	21: {
+		'q':  """
+		""",
+		'a': """
+		""",
+	},
+	22: {
+		'q':  """
+		""",
+		'a': """
+		""",
+	},
+	23: {
+		'q':  """
+		""",
+		'a': """
+		""",
+	},
+	24: {
+		'q':  """
+		""",
+		'a': """
+		""",
+	},
+	25: {
+		'q':  """
+		""",
+		'a': """
+		""",
+	},
+	26: {
+		'q':  """
+		""",
+		'a': """
+		""",
+	},
+	27: {
+		'q':  """
+		""",
+		'a': """
+		""",
+	},
+	28: {
+		'q':  """
+		""",
+		'a': """
+		""",
+	},
+	29: {
+		'q':  """
+		""",
+		'a': """
+		""",
+	},
+	30: {
+		'q':  """
+		""",
+		'a': """
+		""",
+	},
+	31: {
+		'q':  """
+		""",
+		'a': """
+		""",
+	},
+	32: {
+		'q':  """
+		""",
+		'a': """
+		""",
+	},
+	33: {
+		'q':  """
+		""",
+		'a': """
+		""",
+	},
+	34: {
+		'q':  """
+		""",
+		'a': """
+		""",
+	},
+	35: {
+		'q':  """
+		""",
+		'a': """
+		""",
+	},
+	36: {
+		'q':  """
+		""",
+		'a': """
+		""",
+	},
+	37: {
+		'q':  """
+		""",
+		'a': """
+		""",
+	},
+	38: {
+		'q':  """
+		""",
+		'a': """
+		""",
+	},
+	39: {
+		'q':  """
+		""",
+		'a': """
+		""",
+	},
+	40: {
+		'q':  """
+		""",
+		'a': """
+		""",
+	},
+	41: {
+		'q':  """
+		""",
+		'a': """
+		""",
+	},
+	42: {
+		'q':  """
+		""",
+		'a': """
+		""",
+	},
+	43: {
+		'q':  """
+		""",
+		'a': """
+		""",
+	},
+	44: {
+		'q':  """
+		""",
+		'a': """
+		""",
+	},
+	45: {
+		'q':  """
+		""",
+		'a': """
+		""",
+	},
+	46: {
+		'q':  """
+		""",
+		'a': """
+		""",
+	},
+	47: {
+		'q':  """
+		""",
+		'a': """
+		""",
+	},
+	48: {
+		'q':  """
+		""",
+		'a': """
+		""",
+	},
+	49: {
+		'q':  """
+		""",
+		'a': """
+		""",
+	},
+	50: {
+		'q':  """
+		""",
+		'a': """
+		""",
+	},
+	51: {
+		'q':  """
+		""",
+		'a': """
+		""",
+	},
+	52: {
+		'q':  """
+		""",
+		'a': """
+		""",
+	},
+	53: {
+		'q':  """
+		""",
+		'a': """
+		""",
+	},
+	54: {
+		'q':  """
+		""",
+		'a': """
+		""",
+	},
+	55: {
+		'q':  """
+		""",
+		'a': """
+		""",
+	},
+	56: {
+		'q':  """
+		""",
+		'a': """
+		""",
+	},
+	57: {
+		'q':  """
+		""",
+		'a': """
+		""",
+	},
+	58: {
+		'q':  """
+		""",
+		'a': """
+		""",
+	},
+	59: {
+		'q':  """
+		""",
+		'a': """
+		""",
+	},
+	60: {
+		'q':  """
+		""",
+		'a': """
+		""",
+	},
+	61: {
+		'q':  """
+		""",
+		'a': """
+		""",
+	},
+	62: {
+		'q':  """
+		""",
+		'a': """
+		""",
+	},
+	63: {
+		'q':  """
+		""",
+		'a': """
+		""",
+	},
+	64: {
+		'q':  """
+		""",
+		'a': """
+		""",
+	},
+	65: {
+		'q':  """
+		""",
+		'a': """
+		""",
+	},
+	66: {
+		'q':  """
+		""",
+		'a': """
+		""",
+	},
+	67: {
+		'q':  """
+		""",
+		'a': """
+		""",
+	},
+	68: {
+		'q':  """
+		""",
+		'a': """
+		""",
+	},
+	69: {
+		'q':  """
+		""",
+		'a': """
+		""",
+	},
+	70: {
+		'q':  """
+		""",
+		'a': """
+		""",
+	},
+	71: {
+		'q':  """
+		""",
+		'a': """
+		""",
+	},
+	72: {
+		'q':  """
+		""",
+		'a': """
+		""",
+	},
+	73: {
+		'q':  """
+		""",
+		'a': """
+		""",
+	},
+	74: {
+		'q':  """
+		""",
+		'a': """
+		""",
+	},
+	75: {
+		'q':  """
+		""",
+		'a': """
+		""",
+	},
+	76: {
+		'q':  """
+		""",
+		'a': """
+		""",
+	},
+	77: {
+		'q':  """
+		""",
+		'a': """
+		""",
+	},
+	78: {
+		'q':  """
+		""",
+		'a': """
+		""",
+	},
+	79: {
+		'q':  """
+		""",
+		'a': """
+		""",
+	},
+	80: {
+		'q':  """
+		""",
+		'a': """
+		""",
+	},
+	81: {
+		'q':  """
+		""",
+		'a': """
+		""",
+	},
+	82: {
+		'q':  """
+		""",
+		'a': """
+		""",
+	},
+	83: {
+		'q':  """
+		""",
+		'a': """
+		""",
+	},
+	84: {
+		'q':  """
+		""",
+		'a': """
+		""",
+	},
+	85: {
+		'q':  """
+		""",
+		'a': """
+		""",
+	},
+	86: {
+		'q':  """
+		""",
+		'a': """
+		""",
+	},
+	87: {
+		'q':  """
+		""",
+		'a': """
+		""",
+	},
+	88: {
+		'q':  """
+		""",
+		'a': """
+		""",
+	},
+	89: {
+		'q':  """
+		""",
+		'a': """
+		""",
+	},
+	90: {
+		'q':  """
+		""",
+		'a': """
+		""",
+	},
+	91: {
+		'q':  """
+		""",
+		'a': """
+		""",
+	},
+	92: {
+		'q':  """
+		""",
+		'a': """
+		""",
+	},
+	93: {
+		'q':  """
+		""",
+		'a': """
+		""",
+	},
+	94: {
+		'q':  """
+		""",
+		'a': """
+		""",
+	},
+	95: {
+		'q':  """
+		""",
+		'a': """
+		""",
+	},
+	96: {
+		'q':  """
+		""",
+		'a': """
+		""",
+	},
+	97: {
+		'q':  """
+		""",
+		'a': """
+		""",
+	},
+	98: {
+		'q':  """
+		""",
+		'a': """
+		""",
+	},
+	99: {
+		'q':  """
+		""",
+		'a': """
+		""",
+	},
+	100: {
+		'q':  """
+		""",
+		'a': """
+		""",
+	},
+}
