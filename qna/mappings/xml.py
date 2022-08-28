4f7cd865a8f7c131c5080db9a49e46b389c0db75
Noorudin Buraleh <noorudin.buraleh@gmail.com>
2022-08-28 19:20:30 +0100

    main: XML qna file

--- qna/mappings/xml.py
+++ qna/mappings/xml.py
@@ -112,114 +112,231 @@ qna = {
 	},
     8: {
 		'q':  """
-		
+		a) What does an XML attribute represent?
+		b) How many attributes can be associated with a given XML element
 		""",
 		'a': """
-		
+		a) An XML attribute of specifies a single property for the element, using a name/value pair.
+		b) 0..N
 		""",
 	},
     9: {
 		'q':  """
-		
+		Consider the following snippet:
+
+			<elementExample attributeName1="x" attributeName2= "y" attributeName1= "z">....</elementExample>
+
+		Identify and explain what is wrong with this snippet.
 		""",
 		'a': """
-		
+		Attribute 'attributeName1' is specified twice with two different values.
+		This is illegal XML.
 		""",
 	},
     10: {
 		'q':  """
-		
+		True or false:
+			1) Attribute names are defined without quotation marks
+			2) Attribute values do not have be defined with quotation marks  
 		""",
 		'a': """
-		
+			1) True
+			2) False, attribute values must always be defined with quotation marks.
 		""",
 	},
 	11: {
 		'q':  """
-		
+		For each of the given characters, state their corresponding entity
+		reference:
+			a) <
+			b) >
+			c) &
+			d) '
+			e) "
 		""",
 		'a': """
-		
+			a) &lt;
+			b) &gt;
+			c) &amp;
+			d) &apos;
+			e) &quot;
 		""",
 	},
 	12: {
 		'q':  """
-		
+		Consider the following XML Prolog:
+			<?xml version = "version_number" encoding = "encoding_declaration" standalone = "standalone_status" ?>
+
+		Explain the usage of each of the following attribute names:
+			- version
+			- encoding
+			- standalone
 		""",
 		'a': """
-		
+		Version:
+			- Specifies the version of the XML standard to use
+			- E.g "1.0"
+
+		Encoding:
+			- Defines the character encoding used in the document
+			- UTF-8 is the default encoding
+
+		Standalone:
+			- Informs the parser whether the document relies on information
+			  from an external source for its content
+			- E.g Document type definition 'DTD'
+			- Default value is set to 'no'
 		""",
 	},
 	13: {
 		'q':  """
-		
+		What is the formal term for the text that appears between the start and end
+		tags?
+
+		E.g:
+			<tag>what_am_i</tag>
 		""",
 		'a': """
-		
+		The formal term is 'content'.
 		""",
 	},
 	14: {
 		'q':  """
-		
+		Consider the following snippet of an element with no content i.e is empty:
+				<exampleTag></exampleTag>
+
+		a) Simplify the snippet.
+		b) What is the formal term for your answer to (a)?
 		""",
 		'a': """
-		
+		a) <exampleTag />
+		b) Empty-element tag
 		""",
 	},
 	15: {
 		'q':  """
-		
+		What is XPath?
 		""",
 		'a': """
-		
+		XPath is a query language for navigating XML documents.
 		""",
 	},
 	16: {
 		'q':  """
-		
+		Consider the following snippet:
+
+			<?xml version="1.0" encoding="UTF-8" standalone="no" ?>
+			<bookstore>
+				<book category="cooking">
+					<title lang="en">Everyday Italian</title>
+					<author>Giada de Laurentiis</author>
+					<year>2005</year>
+					<price>30.00</price>
+				</book>
+
+				<book category="children">
+					<title lang="en">Harry Potter</title>
+					<author>J.K. Rowling</author>
+					<year>2005</year>
+					<price>29.99</price>
+				</book>
+
+			</bookstore>
+
+		Extract the text of the 'price' element in the second 'book' element using
+		an absolute XPath.
 		""",
-		'a': """
-		
+		'a': r"""
+		/bookstore/book[2]/price/text()
 		""",
 	},
 	17: {
 		'q':  """
-		
+		For each of the following state:
+			a) What the acronym stands for
+			b) What it is used for
+
+		1) XSL
+		2) XSLT
 		""",
 		'a': """
-		
+		1)
+			a) Extensible stylesheet language
+			b) XSL is a styling language for XML
+
+		2)
+			a) Extensible stylesheet language transformations
+			b) XSLT is used to transform XML documents into other formats
+				like HTML.
 		""",
 	},
 	18: {
 		'q':  """
-		
+		a) What does XSD stand for?
+		b) What are .xsd files used for?
 		""",
 		'a': """
-		
+		a) XML Schema definition
+		b) And XSD file is used to define what elements and attributes may appear
+		   in an XML document.
 		""",
 	},
 	19: {
 		'q':  """
-		
+		Consider the following snippet:
+
+			<?xml version="1.0" encoding="UTF-8" standalone="no" ?>
+			<bookstore>
+				<book category="cooking">
+					<title lang="en">Everyday Italian</title>
+					<author>Giada de Laurentiis</author>
+				</book>
+				<book category="children">
+					<title lang="en">Harry Potter</title>
+					<author>J.K. Rowling</author>
+				</book>
+				<extraBook>
+					<book category="testing double solidus">
+						<title lang="en">KSI</title>
+						<author>By KSI</author>
+					</book>
+				</extraBook>
+			</bookstore>
+
+		Q: How could I get all 'book' elements (regardless of where they lie in the hierarchy)
+		with XPath?
 		""",
 		'a': """
-		
+		//book
 		""",
 	},
 	20: {
 		'q':  """
-		
+		Consider the following snippet:
+
+			<baseballLeague>
+				<team name="Tigers">
+					<player name="Joe"/>
+				</team>
+				<team name="Saints">
+					<player name="Steve"/>
+				</team>
+			</baseballLeague>
+
+		Q: How would you get the 'team' element with attribute name="Tigers"
+		using XPath?
 		""",
 		'a': """
-		
+		/baseballLeague/team[@name="Tigers"]
 		""",
 	},
 	21: {
 		'q':  """
-		
+		What are the two main uses for XML?
 		""",
 		'a': """
-		
+		1) To represent low-level data e.g. configuration files
+		2) Add metadata to documents e.g italics, bold
 		""",
 	},
 	22: {
