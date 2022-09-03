2aca899cbf16871942a5694aa85aa28430375830
Noorudin Buraleh <noorudin.buraleh@gmail.com>
2022-09-03 18:17:40 +0100

    main: C++ Concurrency - Chapter 1

---
+++ qna/mappings/cpp_concurrency.py
@@ -0,0 +1,1692 @@
+qna = { # Notes taken in a QNA style from Anothony Williams' 'C++ Concurrency in Action'
+    1: { # Chapter 1: Hello, world of concurrency in C++!
+		'q':  """
+		What is meant by concurrency in computer systems?
+		""",
+		'a': """
+		Concurrency in computer systems refers to a single system performing
+		multiple independent activities in parallel, as opposed to sequentially.
+		""",
+	},
+    2: {
+		'q':  """
+		In the context of computer systems, what is meant by 'task switching'?
+		""",
+		'a': """
+		'Task switching' is a phenomenon that occurs when a machine processor can only perform
+		one task at a time, but can switch between tasks many times per second. So much so
+		that this gives the illusion of concurrency.
+		""",
+	},
+    3: {
+		'q':  """
+		What is hardware concurrency?
+		""",
+		'a': """
+		Hardware concurrency is when computers with:
+			1) multiple processors, or;
+			2) multiple cores within a processor; or
+			3) both (1) and (2)
+
+		are genuinely capable of running more than one task in parallel
+		(i.e not via task switching).
+		""",
+	},
+    4: {
+		'q':  """
+		Consider:
+			- a single core machine
+			- this machine has to do two tasks via task switching
+			- this machine has to perform a context switch every time it
+			  changes from one task to another
+
+		Outline the steps involved in context switching.
+		""",
+		'a': """
+		In order to perform a context switch, the OS has to:
+			- save the CPU state and instruction pointer for the currently running
+			  task
+			- work out which task to switch to
+			- then reload the CPU state for the task being switched to
+			- then CPU will then (potentially have to) load the memory for the
+			  instructions and data for the new task into the cache. Note that
+			  this step can prevent the CPU from executing any instructions,
+			  causing further delay. 
+		""",
+	},
+    5: {
+		'q':  """
+		Consider the following setup:
+			- multiple processes
+			- each process has a single thread
+			- the treads communicate between processes.
+
+		a) Name a few interprocess communication channels available to this setup?
+		b) State two downside of such a setup?
+		""",
+		'a': """
+		a) Standard interprocess communication channels include:
+			1) signals
+			2) sockets
+			3) files
+			4) pipes
+		
+		b)
+		Downside 1:
+			Communication between processes (as stated in (a)) is often complicated
+			to set up, or slow, or both.
+
+		Downside 2:
+			There's an inherent overhead in running multiple processes since
+			it takes time to start a process, the operating system must devote 
+			internal resources to managing the process, and so forth. 
+		""",
+	},
+    6: {
+		'q':  """
+		Consider the standard interprocess communication channels:
+			1) signals
+			2) sockets
+			3) files
+			4) pipes
+
+		a) Why are these complicated to set up, or slow, or both?
+		b) What is one upside to your answer in (a)
+		""",
+		'a': """
+		a)	Because operating systems typically provide a lot of protection between
+			processes to avoid one process accidentally modifying data belonging
+			to another process.
+
+		b)	The added protection operating systems provide between processes
+			mean that it can be easier to write safe concurrent code with processes
+			rather than threads. 
+		""",
+	},
+    7: {
+		'q':  """
+		Name an interesting advantage of using separate proceses for concurrency?
+		""",
+		'a': """
+		You can run the separate processes on distinct machines connected over 
+		a network.
+		""",
+	},
+    8: {
+		'q':  """
+		What does 'address space' mean?
+		""",
+		'a': """
+		An address space is a range of accessible addresses in memory
+		that are available for a program or process.
+
+		The memory can be either physical or virtual and is used for
+		executing instructions and storing data.
+		""",
+	},
+    9: {
+		'q':  """
+		Outline the difference between concurrency and parallelism from
+		a development concern perspective.
+		""",
+		'a': """
+		Parallelism:
+			People talk about parallelism when their primary concern is
+			taking advantage of the available hardware to increase the performance
+			of bulk data processing.
+
+		Concurrency:
+			People talk about concurrency when their primary concern is:
+				- separation of concerns
+				- responsiveness
+		""",
+	},
+    10: {
+		'q':  """
+		Breifly summarise what is meant by 'throughput'.
+		""",
+		'a': """
+		Throughput is a measure of how many units of information a system can
+		process in a given amount of time.
+		""",
+	},
+	11: {
+		'q':  """
+		Briefly outline the difference between:
+			1) throughput
+			2) bandwidth
+			3) latency
+		""",
+		'a': """
+		1) Throughput:
+				Expressed the amount of data that can be processed in a given amount
+				of time.
+
+		2) Bandwidth:
+				Refers to the capacity of the network for data to be moved
+				at one time.
+
+		3) Latency:
+				Is an expression of how much time it takes for a data packet
+				to get from one designated point to another.
+		""",
+	},
+	12: {
+		'q':  """
+		What is meant by the 'abstraction penalty'?
+		""",
+		'a': """
+		The abstraction penalty is the cost associated with using high-level
+		facilities that wrap low-level facilities, as opposed to using the underlying
+		low-level facilities directly.
+		""",
+	},
+	13: {
+		'q':  """
+		Consider the statement:
+			Every thread must has an initial function (i.e a callback/function pointer passed in to the thread constructor).
+			This initial function is where the new thread of execution begins.
+
+		What is the initial function for the initial thread spawned when an application is run?
+		""",
+		'a': """
+		`main()`
+		""",
+	},
+	14: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	15: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	16: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	17: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	18: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	19: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	20: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	21: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	22: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	23: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	24: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	25: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	26: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	27: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	28: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	29: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	30: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	31: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	32: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	33: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	34: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	35: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	36: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	37: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	38: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	39: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	40: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	41: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	42: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	43: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	44: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	45: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	46: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	47: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	48: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	49: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	50: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	51: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	52: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	53: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	54: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	55: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	56: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	57: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	58: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	59: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	60: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	61: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	62: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	63: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	64: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	65: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	66: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	67: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	68: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	69: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	70: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	71: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	72: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	73: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	74: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	75: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	76: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	77: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	78: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	79: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	80: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	81: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	82: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	83: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	84: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	85: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	86: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	87: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	88: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	89: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	90: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	91: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	92: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	93: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	94: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	95: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	96: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	97: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	98: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	99: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	100: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	101: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+    102: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+    103: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+    104: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+    105: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+    106: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+    107: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+    108: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+    109: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+    110: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	111: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	112: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	113: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	114: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	115: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	116: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	117: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	118: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	119: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	120: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	121: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	122: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	123: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	124: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	125: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	126: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	127: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	128: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	129: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	130: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	131: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	132: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	133: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	134: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	135: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	136: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	137: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	138: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	139: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	140: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	141: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	142: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	143: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	144: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	145: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	146: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	147: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	148: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	149: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	150: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	151: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	152: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	153: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	154: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	155: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	156: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	157: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	158: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	159: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	160: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	161: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	162: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	163: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	164: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	165: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	166: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	167: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	168: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	169: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	170: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	171: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	172: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	173: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	174: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	175: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	176: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	177: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	178: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	179: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	180: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	181: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	182: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	183: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	184: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	185: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	186: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	187: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	188: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	189: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	190: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	191: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	192: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	193: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	194: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	195: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	196: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	197: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	198: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	199: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+	200: {
+		'q':  """
+		
+		""",
+		'a': """
+		
+		""",
+	},
+}
