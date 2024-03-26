31e90e3a3257fc05fd2dc4cd36cbba2e645dabd0
Noorudin Buraleh <noorudin.buraleh@gmail.com>
2024-03-26 12:00:51 +0000

    SQUIZ-7: Initial QuestionForm design

---
+++ SquizApp/SquizApp/QuestionForm.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace SquizApp
+{
+    partial class QuestionForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            questionLabel = new Label();
+            textBox1 = new TextBox();
+            SuspendLayout();
+            // 
+            // questionLabel
+            // 
+            questionLabel.AutoSize = true;
+            questionLabel.Font = new Font("Segoe UI", 24F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            questionLabel.Location = new Point(28, 24);
+            questionLabel.Name = "questionLabel";
+            questionLabel.Size = new Size(324, 45);
+            questionLabel.TabIndex = 0;
+            questionLabel.Text = "Question Placeholder";
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Segoe UI", 24F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox1.Location = new Point(28, 153);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(530, 50);
+            textBox1.TabIndex = 1;
+            // 
+            // QuestionForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(textBox1);
+            Controls.Add(questionLabel);
+            Name = "QuestionForm";
+            Text = "Question Form";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label questionLabel;
+        private TextBox textBox1;
+    }
+}
