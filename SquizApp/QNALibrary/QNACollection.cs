33d826b4f6f2b1d91ddc21f7beb30311e95c3695
Noorudin Buraleh <noorudin.buraleh@gmail.com>
2024-05-25 09:25:04 +0100

    SQUIZ-7: Add serious of properties, member functions in QNACollection class

--- SquizApp/QNALibrary/QNACollection.cs
+++ SquizApp/QNALibrary/QNACollection.cs
@@ -1,14 +1,80 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace QNALibrary;
 
-using QNACollectionMapping = Dictionary<int, QNABase>;
+using QNACollectionMapping = Dictionary<string, QNABase>;
 
 public class QNACollection
 {
-    QNACollectionMapping qnaCollectionMapping;
+    public static QNACollectionMapping qnaCollectionMapping = new();
+    public QNACollection()
+    {
+        Add(new Gregoire.Gregoire());   
+    }
+
+    private void Add(QNABase qnaBase)
+    {
+        qnaCollectionMapping.Add(qnaBase.ToString(), qnaBase);
+    }
+
+    public Dictionary<int, Dictionary<string, string>> qnaMapping(string qnaKey)
+    {
+        return qnaCollectionMapping[qnaKey].QNAMapping;
+    }
+
+    //public Dictionary<int,Dictionary<string, string>> GetRandomSubcollection(int nQNA, string qnaKey)
+    //{
+    //    Dictionary<int, Dictionary<string, string>> localQNAMapping = qnaMapping(qnaKey);
+
+    //    // Validate the input size
+    //    if (nQNA < 1)
+    //    {
+    //        throw new ArgumentException("Size must be at least 1.");
+    //    }
+
+    //    if (nQNA > localQNAMapping.Count)
+    //    {
+    //        throw new ArgumentException($"Size must not exceed the size of the collection. Collection size: {localQNAMapping.Count}");
+    //    }
+
+    //    // Get a randomized subcollection of the specified size
+    //    var random = new Random();
+    //    return localQNAMapping
+    //        .OrderBy(x => random.Next())
+    //        .Take(nQNA)
+    //        .ToDictionary(pair => pair.Key, pair => pair.Value);
+    //}
+
+    public Queue<Dictionary<string, string>> GetRandomSubcollection(int nQNA, string qnaKey)
+    {
+        Dictionary<int, Dictionary<string, string>> localQNAMapping = qnaMapping(qnaKey);
+
+        // Validate the input size
+        if (nQNA < 1)
+        {
+            throw new ArgumentException("Size must be at least 1.");
+        }
+
+        if (nQNA > localQNAMapping.Count)
+        {
+            throw new ArgumentException($"Size must not exceed the size of the collection. Collection size: {localQNAMapping.Count}");
+        }
+
+        // Get a randomized subcollection of the specified size
+        var random = new Random();
+        var randomSubcollection = localQNAMapping
+            .OrderBy(x => random.Next())
+            .Take(nQNA)
+            .Select(pair => pair.Value)
+            .ToList();
+
+        // Create a Queue from the selected subcollection
+        return new Queue<Dictionary<string, string>>((IEnumerable<Dictionary<string, string>>)randomSubcollection);
+    }
 }
