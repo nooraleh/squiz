5ce2817faa5f73c5e7f451cd7b91462b2506804c
Noorudin Buraleh <noorudin.buraleh@gmail.com>
2024-05-25 10:25:33 +0100

    SQUIZ-7: Pass SquizManager singleton's CurrentQNA member to QuestionForm ctor in startQuizButton_Click

--- SquizApp/SquizApp/MainForm.cs
+++ SquizApp/SquizApp/MainForm.cs
@@ -1,3 +1,5 @@
+using QNALibrary;
+
 namespace SquizApp
 {
     public partial class MainForm : Form
@@ -9,10 +11,40 @@ namespace SquizApp
 
         private void startQuizButton_Click(object sender, EventArgs e)
         {
+            //qnaDropdownComboBox.Text
+            //QNACollection qnaCollection = new();
+
+            //Dictionary<int, Dictionary<string, string>> liveQNA = qnaCollection.GetRandomSubcollection(Int32.Parse(numberOfQuestionsAskedNumericUpDown.Text), qnaDropdownComboBox.Text);
+            SquizManager.Instance.Setup(Int32.Parse(numberOfQuestionsAskedNumericUpDown.Text), qnaDropdownComboBox.Text);
+
+            //while (SquizManager.Instance.QNASubmapping.Count != 0)
+            //{
+            //    QuestionForm questionForm = new(SquizManager.Instance.CurrentQNA);
+            //    questionForm.FormClosed += (sender, e) => this.Close();
+            //    questionForm.Show();
+            //}
             this.Hide();
-            QuestionForm questionForm = new(); // TODO: Pass in generated questions
+            QuestionForm questionForm = new(SquizManager.Instance.CurrentQNA);
             questionForm.FormClosed += (sender, e) => this.Close();
             questionForm.Show();
+
+
+            //foreach(var qna in liveQNA)
+            //{
+            //    QuestionForm questionForm = new(qna.Value);
+            //    questionForm.FormClosed += (sender, e) => this.Close();
+            //    questionForm.Show();
+            //}
+
+            //this.Hide();
+            //QuestionForm questionForm = new(); // TODO: Pass in generated questions
+            //questionForm.FormClosed += (sender, e) => this.Close();
+            //questionForm.Show();
+        }
+
+        private void generateQNAList(string qnaDropdownComboBoxText, int numberOfQuestionsAskedNumericUpDown)
+        {
+
         }
     }
 }
