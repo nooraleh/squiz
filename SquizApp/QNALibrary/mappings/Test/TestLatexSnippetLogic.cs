8c2f73ed566f014c3afab9cd3eb8334e8c2fa562
Noorudin Buraleh <noorudin.buraleh@gmail.com>
2024-08-24 14:20:03 +0100

    SQUIZ-7: Test mapping to test LaTex snippet generation

---
+++ SquizApp/QNALibrary/mappings/Test/TestLatexSnippetLogic.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QNALibrary.mappings.Test
+{
+    public class TestLatexSnippetLogic : QNABase
+    {
+        public Gregoire()
+        : base(title: "TestLatexSnippetLogic", category: QNACategory.CPP, qnaMapping: qnaMapping_)
+        { }
+
+        public override string ToString()
+        {
+            return "TestLatexSnippetLogic";
+        }
+
+        static Dictionary<int, Dictionary<string, string>> qnaMapping_ = new Dictionary<int, Dictionary<string, string>>()
+        {
+            {1, new Dictionary<string, string>()
+                {
+                    { "q", @"
+i) What is an initializer-list constructor?
+ii) Let EvenSquence be a proposed class. Write the sequence for an
+EvenSequence initializer-list constructor for arguments of type double." },
+                    { "a", @"
+i) An initializer-list constructor is a construct with only std::initializer_list<T> as first parameter." },
+                    {"snippetA", @"
+#include <initializer_list>
+class EvenSequence
+{
+public:
+	EvenSequence(std::initializer_list<double> args)
+	{
+	}
+};"                 
+                    }
+                }
+            },
+        };
+    }
+}
