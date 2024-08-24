274be302164193c6e5df1daf7ba17407396ae3b0
Noorudin Buraleh <noorudin.buraleh@gmail.com>
2024-08-24 13:53:52 +0100

    SQUIZ-7: Add utility functions for compiling, displaying LaTeX documents as PDFs

--- SquizApp/QNALibrary/Utility.cs
+++ SquizApp/QNALibrary/Utility.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
@@ -69,6 +70,43 @@ ${0}$
             return string.Format(latexTemplate, codeSnippet);
         }
 
+        public static string CompileLatexDocumentToPDF(string textFilePath, string latexDocument)
+        {
+            // textFilePath will be based on the QNA in question
+
+            return string.Empty;
+        }
+
+        public static void DisplayLatexPDF(string texFilePath, string latexCode)
+        {
+            // to be called in the 'displaySnippetButton_OnClick' event handler
+            File.WriteAllText(texFilePath, latexCode);
+
+            // Compile the .tex file to a PDF using pdflatex with -shell-escape option
+            ProcessStartInfo psi = new ProcessStartInfo();
+            psi.FileName = "pdflatex";
+            psi.Arguments = $"-shell-escape -interaction=nonstopmode {texFilePath}";
+            psi.RedirectStandardOutput = true;
+            psi.RedirectStandardError = true;
+            psi.UseShellExecute = false;
+
+            Process process = Process.Start(psi);
+            process.WaitForExit();
+        }
+
+        public static void DisplayLatexPDF(string texFilePath)
+        {
+            // Display the generated PDF (using default PDF viewer)
+            string pdfFilePath = Path.ChangeExtension(texFilePath, ".pdf");
+            if (File.Exists(pdfFilePath))
+            {
+                Process.Start(new ProcessStartInfo(pdfFilePath) { UseShellExecute = true });
+            }
+            else
+            {
+                Console.WriteLine("PDF generation failed.");
+            }
+        }
 
     }
 }
