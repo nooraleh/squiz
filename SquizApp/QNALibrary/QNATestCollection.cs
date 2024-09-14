85242a5a97d9575719127b9c4e80537b60ee12e5
Noorudin Buraleh <noorudin.buraleh@gmail.com>
2024-09-14 13:45:30 +0100

    SQUIZ-12: Initial QNATestCollection class

---
+++ SquizApp/QNALibrary/QNATestCollection.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QNALibrary;
+
+public class QNATestCollection : QNACollection
+{
+    public QNATestCollection()
+    {
+        base.Add(new mappings.Test.TestLatexSnippetLogic());
+    }
+
+}
