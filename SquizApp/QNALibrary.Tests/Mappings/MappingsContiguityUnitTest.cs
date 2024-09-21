19316e520fb7856f1d38af3d8b34ac14a33f280b
Noorudin Buraleh <noorudin.buraleh@gmail.com>
2024-09-21 11:56:59 +0100

    SQUIZ-16: Add unit tests for testing contiguity of QNA index ids in MappingsContiguityUnitTest.cs

--- SquizApp/QNALibrary.Tests/Mappings/MappingsContiguityUnitTest.cs
+++ SquizApp/QNALibrary.Tests/Mappings/MappingsContiguityUnitTest.cs
@@ -1,5 +1,10 @@
 ﻿using FluentAssertions;
 using QNALibrary.mappings.C;
+using QNALibrary.mappings.CPP;
+using QNALibrary.mappings.CSharp;
+using QNALibrary.mappings.Finance;
+using QNALibrary.mappings.Software;
+using QNALibrary.mappings.Template;
 
 
 namespace QNALibrary.Tests.Mappings
@@ -29,10 +34,126 @@ namespace QNALibrary.Tests.Mappings
         public void QNALibrary_mappings_C_CBasics()
         {
             var result = MappingsContiguityUnitTest.GetMissingKeys(new CBasics());
-
             result.Should().BeEmpty();
         }
 
+        [Fact]
+        public void QNALibrary_mappings_CPP_BoostAsio()
+        {
+            var result = MappingsContiguityUnitTest.GetMissingKeys(new BoostAsio());
+            result.Should().BeEmpty();
+        }
+ 
+        [Fact]
+        public void QNALibrary_mappings_CPP_CiscoAdvancedCP()
+        {
+            var result = MappingsContiguityUnitTest.GetMissingKeys(new CiscoAdvancedCPP());
+            result.Should().BeEmpty();
+        }
 
+        [Fact]
+        public void QNALibrary_mappings_CPP_CPPBasics()
+        {
+            var result = MappingsContiguityUnitTest.GetMissingKeys(new CPPBasics());
+            result.Should().BeEmpty();
+        }
+
+        [Fact]
+        public void QNALibrary_mappings_CPP_CPPConcurrency()
+        {
+            var result = MappingsContiguityUnitTest.GetMissingKeys(new CPPConcurrency());
+            result.Should().BeEmpty();
+        }
+
+        [Fact]
+        public void QNALibrary_mappings_CPP_CPPSTL()
+        {
+            var result = MappingsContiguityUnitTest.GetMissingKeys(new CPPSTL());
+            result.Should().BeEmpty();
+        }
+
+        [Fact]
+        public void QNALibrary_mappings_CPP_CPPYouTube()
+        {
+            var result = MappingsContiguityUnitTest.GetMissingKeys(new CPPYouTube());
+            result.Should().BeEmpty();
+        }
+
+        [Fact]
+        public void QNALibrary_mappings_CPP_DesignPatternsCPP20()
+        {
+            var result = MappingsContiguityUnitTest.GetMissingKeys(new DesignPatternsCPP20());
+            result.Should().BeEmpty();
+        }
+
+        [Fact]
+        public void QNALibrary_mappings_CPP_EffectiveCPP11_14()
+        {
+            var result = MappingsContiguityUnitTest.GetMissingKeys(new EffectiveCPP11_14());
+            result.Should().BeEmpty();
+        }
+
+        [Fact]
+        public void QNALibrary_mappings_CPP_Gregoire()
+        {
+            var result = MappingsContiguityUnitTest.GetMissingKeys(new Gregoire());
+            result.Should().BeEmpty();
+        }
+
+        [Fact]
+        public void QNALibrary_mappings_CSharp_CS11DotNet7()
+        {
+            var result = MappingsContiguityUnitTest.GetMissingKeys(new CS11DotNet7());
+            result.Should().BeEmpty();
+        }
+
+        [Fact]
+        public void QNALibrary_mappings_CSharp_CSharpIntermediate()
+        {
+            var result = MappingsContiguityUnitTest.GetMissingKeys(new CSharpIntermediate());
+            result.Should().BeEmpty();
+        }
+
+        [Fact]
+        public void QNALibrary_mappings_CSharp_DotNetMaui()
+        {
+            var result = MappingsContiguityUnitTest.GetMissingKeys(new DotNetMaui());
+            result.Should().BeEmpty();
+        }
+
+        [Fact]
+        public void QNALibrary_mappings_Finance_KeyFinancialMarketsConcepts()
+        {
+            var result = MappingsContiguityUnitTest.GetMissingKeys(new KeyFinancialMarketsConcepts());
+            result.Should().BeEmpty();
+        }
+
+        [Fact]
+        public void QNALibrary_mappings_Finance_OptionsFuturesOtherDerivatives()
+        {
+            var result = MappingsContiguityUnitTest.GetMissingKeys(new OptionsFuturesOtherDerivatives());
+            result.Should().BeEmpty();
+        }
+
+        [Fact]
+        public void QNALibrary_mappings_Finance_PracticalCPP20Finance()
+        {
+            var result = MappingsContiguityUnitTest.GetMissingKeys(new PracticalCPP20Finance());
+            result.Should().BeEmpty();
+        }
+
+        [Fact]
+        public void QNALibrary_mappings_Software_NetworkProgramming()
+        {
+            var result = MappingsContiguityUnitTest.GetMissingKeys(new NetworkProgramming());
+            result.Should().BeEmpty();
+        }
+
+        [Fact]
+        public void QNALibrary_mappings_Template_Template()
+        {
+            var result = MappingsContiguityUnitTest.GetMissingKeys(new Template());
+            result.Should().BeEmpty();
+        }
     }
 }
