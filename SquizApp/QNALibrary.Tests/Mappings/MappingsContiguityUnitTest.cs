7a2b14b9e6c4bf2f05ab0f01bf065bbb7b81ca5d
Noorudin Buraleh <noorudin.buraleh@gmail.com>
2024-09-21 09:59:06 +0100

    SQUIZ-16: Add MappingsContiguityUnitTest

---
+++ SquizApp/QNALibrary.Tests/Mappings/MappingsContiguityUnitTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QNALibrary.Tests.Mappings
+{
+    public class MappingsContiguityUnitTest
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
