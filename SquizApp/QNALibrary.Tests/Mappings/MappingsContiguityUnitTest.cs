791f363c3ae157b19c96f1e901dfbeab49db6977
Noorudin Buraleh <noorudin.buraleh@gmail.com>
2024-09-21 10:10:45 +0100

    SQUIZ-16: Helper function to get the missing keys of a QNAMapping from a QNABase instance

--- SquizApp/QNALibrary.Tests/Mappings/MappingsContiguityUnitTest.cs
+++ SquizApp/QNALibrary.Tests/Mappings/MappingsContiguityUnitTest.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using QNALibrary.mappings.C;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,9 +9,31 @@ namespace QNALibrary.Tests.Mappings
 {
     public class MappingsContiguityUnitTest
     {
-        [Fact]
-        public void Test1()
+        public static List<int> GetMissingKeys(QNABase qnaBase)
         {
+            List<int> missingKeys = new List<int>();
+
+            int minKey = 1;
+            int maxKey = qnaBase.QNAMapping.Keys.Count > 0 ? qnaBase.QNAMapping.Keys.Max() : 0;
+
+            // loop from minKey to maxKey and check for gaps
+            for (int i = minKey; i <= maxKey; i++)
+            {
+                if (!qnaBase.QNAMapping.ContainsKey(i))
+                {
+                    missingKeys.Add(i);
+                }
+            }
+
+            return missingKeys;
+        }
+
+
+        [Fact]
+        public void QNALibrary_mappings_C_CBasics()
+        {
+            CBasics cBasisQNABase = new();
+
 
         }
     }
